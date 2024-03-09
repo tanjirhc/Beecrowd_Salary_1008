9cadad1955e4c277350ecee6927577a8310e5188
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-10 00:47:43 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd_Salary_1008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, Hour;
+            double Amount, Salary;
+            n = Convert.ToInt32(Console.ReadLine());
+            Hour = Convert.ToInt32(Console.ReadLine());
+            Amount = Convert.ToDouble(Console.ReadLine());
+            
+            Salary = Hour * Amount;
+            Console.WriteLine("NUMBER = " + n);
+            Console.WriteLine("SALARY = " + Salary);
+            Console.ReadKey();
+        }
+    }
+}
